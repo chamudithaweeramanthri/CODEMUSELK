a0b45aa265ecd82f543c40336a312148935987be
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2026-03-02 01:17:23 +0530

    add navbar.

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import logo from "./assets/Logo.png";
 
-const NavBar = () => {
+const Navbar: React.FC = () => {
   return (
-    <div className='bg-red-500 w-full h-16'>NavBar</div>
-  )
-}
+    <nav className="fixed w-full flex items-center justify-between px-8 py-4">
+      {/* Logo */}
+      <div className="text-2xl font-bold">
+        <img src={logo} alt="CODEMUSE Logo" className="h-8 w-auto" />
+      </div>
 
-export default NavBar
+      {/* Menu Items */}
+      <ul className="flex gap-8 text-white text-lg">
+        <li className="cursor-pointer hover:text-gray-300 transition">Home</li>
+        <li className="cursor-pointer hover:text-gray-300 transition">Services</li>
+        <li className="cursor-pointer hover:text-gray-300 transition">Portfolio</li>
+        <li className="cursor-pointer hover:text-gray-300 transition">Clients</li>
+        <li className="cursor-pointer hover:text-gray-300 transition">Contact Us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import NavBar from './NavBar'
 import Hero from './Hero'
@@ -8,9 +9,11 @@ import Team from './Team'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NavBar />
-    <Hero />
-    <Services />
-    <Team />
+    <BrowserRouter>
+      <NavBar />
+      <Hero />
+      <Services />
+      <Team />
+    </BrowserRouter>
   </StrictMode>,
 )

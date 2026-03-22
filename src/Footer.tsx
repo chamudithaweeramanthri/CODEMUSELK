e6576316ca8c81079c5bb632c6d2b7cbb863ce93
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2026-03-22 14:49:30 +0530

    enhance Footer component layout and content, add description and adjust alignment

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,12 +5,14 @@ const Footer = () => {
   return (
     <div className='w-full h-80 bg-[#000000] opacity-98 flex items-center justify-center text-white text-sm almarai-regular'>
       <div className='flex flex-col items-center '>
-        <div className="flex flex-row gap-10 text-[18px]">
+        <div className="flex flex-row gap-10 text-[18px] align-right left-10">
             {/* Logo */}
             <div className='absolute left-10 '>
             <img src={logo} alt="CODEMUSE Logo" className="h-16 w-55" />
+            <p className='text-[15px] mt-2 .abeezee-regular'>Building smart digital solutions <br/> that drive growth, efficiency, and success. </p>
             </div>
 
+          <div className='relative flex flex-row gap-10 left-100'>
             <div className='relative flex flex-col gap-5 top-5'>
               <a href="#" className='hover:text-gray-400 transition'>Home</a>
               <a href="#" className='hover:text-gray-400 transition'>Services</a>
@@ -30,14 +32,16 @@ const Footer = () => {
                 <img src="https://img.icons8.com/?size=100&id=uLWV5A9vXIPu&format=png&color=000000" alt="Facebook" className='w-8 h-' />
               </a>
 
+              
+            </div>
+            <div className='relative flex flex-col gap-5 top-5'>
               <a href="#" className='hover:text-gray-400 transition'>
                 <img src="https://img.icons8.com/?size=100&id=ZRiAFreol5mE&format=png&color=000000" alt="Instagram" className='w-8 h-' />
               </a>
-              
             </div>
-            
-
+          </div>
         </div>
+
         <div className='relative mt-30 flex flex-col items-center gap-3'>
           <p>© 2026 CODEMUSELK. All rights reserved.</p>
           <div className='flex gap-6'>

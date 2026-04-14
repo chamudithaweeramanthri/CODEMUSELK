95b485e5638aeb1ac97a318e856ca0d3b521fbf9
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2026-04-14 10:42:28 +0530

    refactor Footer component, add social media icons and adjust layout

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -9,7 +9,20 @@ const Footer = () => {
             {/* Logo */}
             <div className='absolute left-10 '>
             <img src={logo} alt="CODEMUSE Logo" className="h-16 w-55" />
-            <p className='text-[15px] mt-2 .abeezee-regular'>Building smart digital solutions <br/> that drive growth, efficiency, and success. </p>
+            <p className='text-[15px] -mt-2 .abeezee-regular'>Building smart digital solutions <br/> that drive growth, efficiency, and success. </p>
+            {/* Socialmedia icons */}
+            <div className='flex gap-4 mt-4'>
+              <a href="#" className='hover:text-gray-400 transition'>
+                <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="LinkedIn" className='w-8 h-8' />
+              </a>
+              <a href="#" className='hover:text-gray-400 transition'>
+                <img src="https://img.icons8.com/?size=100&id=uLWV5A9vXIPu&format=png&color=000000" alt="Facebook" className='w-8 h-8' />
+              </a>
+              <a href="#" className='hover:text-gray-400 transition'>
+                <img src="https://img.icons8.com/?size=100&id=ZRiAFreol5mE&format=png&color=000000" alt="Instagram" className='w-8 h-8' />
+              </a>
+            </div>
+
             </div>
 
           <div className='relative flex flex-row gap-10 left-100'>
@@ -21,26 +34,11 @@ const Footer = () => {
             <div className='relative flex flex-col gap-5 top-5'>
               <a href="#" className='hover:text-gray-400 transition'>Clients</a>
               <a href="#" className='hover:text-gray-400 transition'>Contact Us</a>
-            </div>
-
-            {/* social media icons */}
-            <div className='relative flex flex-col gap-5 top-5'>
-              <a href="#" className='hover:text-gray-400 transition'>
-                <img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" alt="LinkedIn" className='w-8 h-' />
-              </a>
-              <a href="#" className='hover:text-gray-400 transition'>
-                <img src="https://img.icons8.com/?size=100&id=uLWV5A9vXIPu&format=png&color=000000" alt="Facebook" className='w-8 h-' />
-              </a>
-
-              
-            </div>
-            <div className='relative flex flex-col gap-5 top-5'>
-              <a href="#" className='hover:text-gray-400 transition'>
-                <img src="https://img.icons8.com/?size=100&id=ZRiAFreol5mE&format=png&color=000000" alt="Instagram" className='w-8 h-' />
-              </a>
-            </div>
+            </div>  
           </div>
+
         </div>
+
 
         <div className='relative mt-30 flex flex-col items-center gap-3'>
           <p>© 2026 CODEMUSELK. All rights reserved.</p>
--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
       <Services />
       <Projects />
       <Contactus />
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   </StrictMode>,
 )

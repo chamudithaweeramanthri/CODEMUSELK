3f4a75106236d1fb6a06e11a438735b77117416a
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2026-04-15 18:49:05 +0530

    revert from HashRouter to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import {HashRouter} from 'react-router-dom'
 import './index.css'
 import NavBar from './NavBar'
 import Hero from './Hero'
@@ -12,13 +11,13 @@ import Footer from './Footer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <NavBar />
       <Hero />
       <Services />
       <Projects />
       <Contactus />
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )

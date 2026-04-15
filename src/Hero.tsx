ccfadfcb451765a2f4e212fcc970aa4733d3c1a4
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2026-04-15 10:48:29 +0530

    refactor Hero component, integrate framer-motion for enhanced animations

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,15 +1,26 @@
+import {motion} from "framer-motion";
+
 const Hero = () => {
   return (
     <div id="home" className="relative h-screen w-full overflow-hidden scroll-mt-24">
       {/* <div className='bg-blue-500 w-screen h-16'>Hero</div> */}
-      
-      
+
+
+
         {/* gradient effect */}
         <div className="absolute inset-0 flex w-screen h-screen">
           <div className="w-1/2 h-full bg-conic-90 from-[#000000]  to-[#BF00FF]" />
           <div className="w-1/2 h-full bg-conic-270 from-[#BF00FF] to-[#000000]" />
         </div>
-        
+
+        {/* Light ON Effect */}
+        <motion.div
+          className="absolute inset-0 bg-black z-10 "
+          initial={{ opacity: 1 }}     // fully dark
+          animate={{ opacity: 0 }}     // fade to reveal gradient
+          transition={{duration: 2, ease: "easeInOut" }}
+        />
+
         {/* Layer 2 - Middle Gradient */}
         <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
           <div className="w-full h-screen max-w-[800px] bg-linear-180 from-[#ae00ff] to-transparent 
@@ -17,13 +28,33 @@ const Hero = () => {
           </div>
         </div>
 
+        
+
         {/* Hero content */}
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bottom-35 sm:bottom-40 md:top-10">
-          <h1 className="text-[23px] mt-80 leading-6 sm:text-[30px] md:text-[32px] lg:text-[40px] sm:mt-100 md:mt-1 sm:leading-8 md:leading-10 afacad-regular">TRANSFORMING IDEAS INTO DIGITAL REALITY</h1>
-          <p className="text-[13px] mt-3 sm:text-[13px] md:text-[15px] lg:text-[18px] sm:mt-2 max-w-xl almarai-regular">
+          {/* <h1 className="text-[23px] mt-80 leading-6 sm:text-[30px] md:text-[32px] lg:text-[40px] sm:mt-100 md:mt-1 sm:leading-8 md:leading-10 afacad-regular">TRANSFORMING IDEAS INTO DIGITAL REALITY</h1> */}
+          <motion.h1
+            className="text-[23px] mt-80 leading-6 sm:text-[30px] md:text-[32px] lg:text-[40px] sm:mt-100 md:mt-1 sm:leading-8 md:leading-10 afacad-regular"
+            initial={{ opacity: 0, y:0 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.8, duration: 2, ease: "easeInOut" }}
+          >
+            TRANSFORMING IDEAS INTO DIGITAL REALITY
+          </motion.h1>
+
+          {/* <p className="text-[13px] mt-3 sm:text-[13px] md:text-[15px] lg:text-[18px] sm:mt-2 max-w-xl almarai-regular">
             We craft innovative software solutions, AI automations, and digital experiences that empower businesses to thrive in the modern world.
-          </p>
-          
+          </p> */}
+
+          <motion.p
+            className="text-[13px] mt-3 sm:text-[13px] md:text-[15px] lg:text-[18px] sm:mt-2 max-w-xl almarai-regular"
+            initial={{ opacity: 0, y:0 }} 
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 1.2, duration: 2, ease: "easeInOut" }}
+          >
+            We craft innovative software solutions, AI automations, and digital experiences that empower businesses to thrive in the modern world.
+          </motion.p>
+
           {/* Stats section */}
           <div className="relative w-full py-10 sm:py-10 md:py-16 lg:py-20 afacad-bold">
             <div className="max-w-6xl mx-auto px-5 sm:px-5 lg:px-22">

15969b59449c0b79803e0485b049f52bf6eb2dc5
chamudithaweeramanthri <chamudithaweeramanthri@gmail.com>
2026-05-03 11:52:24 +0530

    fix: update contact information in ContactUs component

--- src/ContactUs.tsx
+++ src/ContactUs.tsx
@@ -96,7 +96,7 @@ const ContactUs = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                     </svg>
-                    <h2 className="text-[20px] afacad-bold text-center">Colombo,Sri Lanka</h2>
+                    <h2 className="text-[19px] afacad-bold text-center">Colombo,Sri Lanka</h2>
                     
                 </motion.div>
             
@@ -107,7 +107,7 @@ const ContactUs = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
                     </svg>
 
-                    <h2 className="text-[20px] afacad-bold text-center ">+94 77 123 4567</h2>
+                    <h2 className="text-[19px] afacad-bold text-center ">+94 71 520 2498</h2>
                     
                 </motion.div>
 
@@ -117,7 +117,7 @@ const ContactUs = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="mx-auto mb-4 h-12 w-12 text-[#ffffff]">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                     </svg>
-                    <h2 className="text-[20px] afacad-bold text-center ">contact@codemuselk.com</h2>
+                    <h2 className="text-[19px] afacad-bold text-center ">codemuselk.info@gmail.com</h2>
                     
                 </motion.div>
             </motion.div>
